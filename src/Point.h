601a2495753a3454e5a79b4a949562fa05b2d2c5
PANFACTORY <yutatanabe510@gmail.com>
2022-10-03 23:33:49 +0900

    👍 add operators into Point

--- src/Point.h
+++ src/Point.h
@@ -6,7 +6,6 @@
 //*****************************************************************************
 
 #pragma once
-#include <array>
 #include <cmath>
 
 namespace EllipticalPAN {
@@ -16,30 +15,72 @@ class Point {
     Point() {
         this->x[0] = T();
         this->x[1] = T();
-    };
-    ~Point(){};
+    }
     Point(T _x, T _y) {
         this->x[0] = _x;
         this->x[1] = _y;
     }
+    Point(const Point<T>& point) {
+        this->x[0] = point.x[0];
+        this->x[1] = point.x[1];
+    }
+    ~Point(){};
+
+    bool operator==(const Point<T>& point) const {
+        return this->x[0] == point.x[0] && this->x[1] == point.x[1];
+    }
+    bool operator!=(const Point<T>& point) const { return !(*this == point); }
+
+    Point<T>& operator=(const Point<T>& point) {
+        if (this != &point) {
+            this->x[0] = point.x[0];
+            this->x[1] = point.x[1];
+        }
+        return *this;
+    }
+    Point<T>& operator+=(const Point<T>& point) {
+        this->x[0] += point.x[0];
+        this->x[1] += point.x[1];
+        return *this;
+    }
+    Point<T>& operator-=(const Point<T>& point) {
+        this->x[0] -= point.x[0];
+        this->x[1] -= point.x[1];
+        return *this;
+    }
+    Point<T>& operator*=(T a) {
+        this->x[0] *= a;
+        this->x[1] *= a;
+        return *this;
+    }
+    Point<T>& operator/=(T a) {
+        this->x[0] /= a;
+        this->x[1] /= a;
+        return *this;
+    }
 
     const Point<T> operator+(const Point<T>& point) const {
-        return Point<T>(this->x[0] + point.x[0], this->x[1] + point.x[1]);
+        Point<T> ret = *this;
+        return ret += point;
     }
     const Point<T> operator-(const Point<T>& point) const {
-        return Point<T>(this->x[0] - point.x[0], this->x[1] - point.x[1]);
+        Point<T> ret = *this;
+        return ret -= point;
     }
-    const Point<T> operator*(T _a) const {
-        return Point<T>(this->x[0] * _a, this->x[1] * _a);
+    const Point<T> operator-() const { return Point<T>() - *this; }
+    const Point<T> operator*(T a) const {
+        Point<T> ret = *this;
+        return ret *= a;
     }
-    const Point<T> operator/(T _a) const {
-        return Point<T>(this->x[0] / _a, this->x[1] / _a);
+    const Point<T> operator/(T a) const {
+        Point<T> ret = *this;
+        return ret *= a;
     }
 
     T& operator[](unsigned int index) { return this->x[index]; }
 
-    T Dot(const Point<T>& _point) const {
-        return this->x[0] * _point.x[0] + this->x[1] * _point.x[1];
+    T Dot(const Point<T>& point) const {
+        return this->x[0] * point.x[0] + this->x[1] * point.x[1];
     }
     T Norm() const { return sqrt(this->Dot(*this)); }
 
@@ -47,8 +88,14 @@ class Point {
     T x[2];
 };
 
-template <class T>
-inline Point<T> operator*(T _a, const Point<T>& _point) {
-    return _point * _a;
+template <class U>
+inline std::ostream& operator<<(std::ostream& out, const Point<U>& point) {
+    out << point[0] << std::endl << point[1] << std::endl;
+    return out;
+}
+
+template <class U>
+inline Point<U> operator*(U a, const Point<U>& point) {
+    return point * a;
 }
 }  // namespace EllipticalPAN
