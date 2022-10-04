4ca47b24927082394dba7dc71ea0c01fcbe3d733
PANFACTORY <yutatanabe510@gmail.com>
2022-10-04 21:53:42 +0900

    ♻️ refactor codes

--- src/mesher.h
+++ src/mesher.h
@@ -16,190 +16,124 @@
 
 namespace EllipticalPAN {
 template <class T>
-class EllipticalMesher {
+class Mesher {
    public:
-    EllipticalMesher();
-    ~EllipticalMesher();
-    EllipticalMesher(int _inum, int _jnum, T _DEPS);
+    Mesher() = delete;
+    Mesher(int _inum, int _jnum, T _DEPS = T(1e-8), int _ITRMAX = 100000)
+        : inum(_inum), jnum(_jnum), DEPS(_DEPS), ITRMAX(_ITRMAX) {
+        this->pb = std::vector<Point<T> >(2 * (this->inum + this->jnum - 2));
+        this->pin = std::vector<std::vector<Point<T> > >(
+            this->inum, std::vector<Point<T> >(this->jnum, Point<T>(T(), T())));
+    }
+    Mesher(const Mesher<T>&) = delete;
+    ~Mesher() {}
 
-    void SetBoundaryPoint(int, Point<T>);
-    void MakeMesh();
-    void ExportToVTK(std::string);
-    void ExportForPANSFEM(std::string);
+    void SetPoint(int i, const Point<T>& point) { this->pb[i] = point; }
+    void Generate() {
+        //----------Set Dirichlet condition----------
+        int pbi = 0;
+        //.....Bottom edge.....
+        for (int i = 0; i < this->inum - 1; i++, pbi++) {
+            this->pin[i][0] = this->pb[pbi];
+        }
+        //.....Right edge.....
+        for (int i = 0; i < this->jnum - 1; i++, pbi++) {
+            this->pin[this->inum - 1][i] = this->pb[pbi];
+        }
+        //.....Top edge.....
+        for (int i = this->inum - 1; i > 0; i--, pbi++) {
+            this->pin[i][this->jnum - 1] = this->pb[pbi];
+        }
+        //.....Left edge.....
+        for (int i = this->jnum - 1; i > 0; i--, pbi++) {
+            this->pin[0][i] = this->pb[pbi];
+        }
+
+        //----------Solve Laplace equation----------
+        for (int k = 0; k < this->ITRMAX; k++) {
+            T dxmax = T();
+            for (int i = 1; i < this->inum - 1; i++) {
+                for (int j = 1; j < this->jnum - 1; j++) {
+                    //.....Make Laplace equation.....
+                    Point<T> xxi =
+                        0.5 * (this->pin[i + 1][j] - this->pin[i - 1][j]);
+                    Point<T> xiota =
+                        0.5 * (this->pin[i][j + 1] - this->pin[i][j - 1]);
+                    T alpha = xiota.Dot(xiota) + 1.0e-8;
+                    T beta = xxi.Dot(xiota);
+                    T ganma = xxi.Dot(xxi) + 1.0e-8;
+
+                    //.....Update values.....
+                    Point<T> tmp = this->pin[i][j];
+                    this->pin[i][j] =
+                        (0.5 / (alpha + ganma)) *
+                        (alpha * (this->pin[i + 1][j] + this->pin[i - 1][j]) -
+                         0.5 * beta *
+                             (this->pin[i + 1][j + 1] -
+                              this->pin[i - 1][j + 1] -
+                              this->pin[i + 1][j - 1] +
+                              this->pin[i - 1][j - 1]) +
+                         ganma * (this->pin[i][j + 1] + this->pin[i][j - 1]));
+
+                    //.....Get norm maximam.....
+                    T dxtmp = (tmp - this->pin[i][j]).Norm();
+                    if (dxmax < dxtmp) {
+                        dxmax = dxtmp;
+                    }
+                }
+            }
+            //.....Check convergence.....
+            if (dxmax < DEPS) {
+                std::cout << "Converged:k = " << k + 1;
+                break;
+            }
+        }
+    }
+    void ExportToVTK(std::string _fname) {
+        std::ofstream fout(_fname + ".vtk");
+
+        //----------Headers----------
+        fout << "# vtk DataFile Version 4.1\n";
+        fout << "vtk output\n";
+        fout << "ASCII\n";
+        fout << "DATASET UNSTRUCTURED_GRID\n";
+
+        //----------Points----------
+        fout << "\nPOINTS\t" << this->inum * this->jnum << "\tfloat\n";
+        for (auto pinrow : this->pin) {
+            for (auto pinone : pinrow) {
+                fout << pinone[0] << "\t" << pinone[1] << "\t" << 0.0 << "\n";
+            }
+        }
+
+        //----------Cells----------
+        fout << "\nCELLS " << (this->inum - 1) * (this->jnum - 1) << "\t"
+             << (this->inum - 1) * (this->jnum - 1) * 5 << "\n";
+        for (int i = 0; i < this->inum - 1; i++) {
+            for (int j = 0; j < this->jnum - 1; j++) {
+                fout << "4 " << i * (this->jnum) + j << "\t"
+                     << i * (this->jnum) + j + 1 << "\t"
+                     << (i + 1) * (this->jnum) + j + 1 << "\t"
+                     << (i + 1) * (this->jnum) + j << "\n";
+            }
+        }
+
+        //----------Cell types----------
+        fout << "\nCELL_TYPES\t" << (this->inum - 1) * (this->jnum - 1) << "\n";
+        for (int i = 0; i < (this->inum - 1) * (this->jnum - 1); i++) {
+            fout << "9\n";
+        }
+
+        fout.close();
+    }
 
    private:
-    const int inum;
-    const int jnum;
+    const int inum, jnum;
 
     std::vector<Point<T> > pb;
     std::vector<std::vector<Point<T> > > pin;
 
-    static const int ITRMAX = 100000;
+    const int ITRMAX;
     const T DEPS;
 };
-
-template <class T>
-EllipticalMesher<T>::EllipticalMesher() : inum(0), jnum(0), DEPS(T()) {}
-
-template <class T>
-EllipticalMesher<T>::~EllipticalMesher() {}
-
-template <class T>
-EllipticalMesher<T>::EllipticalMesher(int _inum, int _jnum, T _DEPS)
-    : inum(_inum), jnum(_jnum), DEPS(_DEPS) {
-    this->pb = std::vector<Point<T> >(2 * (this->inum + this->jnum - 2));
-    this->pin = std::vector<std::vector<Point<T> > >(
-        this->inum, std::vector<Point<T> >(this->jnum, Point<T>(T(), T())));
-}
-
-template <class T>
-void EllipticalMesher<T>::SetBoundaryPoint(int _i, Point<T> _point) {
-    this->pb[_i] = _point;
-}
-
-template <class T>
-void EllipticalMesher<T>::MakeMesh() {
-    //----------Set Dirichlet condition----------
-    int pbi = 0;
-    //.....Bottom edge.....
-    for (int i = 0; i < this->inum - 1; i++, pbi++) {
-        this->pin[i][0] = this->pb[pbi];
-    }
-    //.....Right edge.....
-    for (int i = 0; i < this->jnum - 1; i++, pbi++) {
-        this->pin[this->inum - 1][i] = this->pb[pbi];
-    }
-    //.....Top edge.....
-    for (int i = this->inum - 1; i > 0; i--, pbi++) {
-        this->pin[i][this->jnum - 1] = this->pb[pbi];
-    }
-    //.....Left edge.....
-    for (int i = this->jnum - 1; i > 0; i--, pbi++) {
-        this->pin[0][i] = this->pb[pbi];
-    }
-
-    //----------Solve Laplace equation----------
-    for (int k = 0; k < this->ITRMAX; k++) {
-        T dxmax = T();
-        // std::cout << "iteration = " << k + 1;
-
-        for (int i = 1; i < this->inum - 1; i++) {
-            for (int j = 1; j < this->jnum - 1; j++) {
-                //.....Make Laplace equation.....
-                Point<T> xxi =
-                    0.5 * (this->pin[i + 1][j] - this->pin[i - 1][j]);
-                Point<T> xiota =
-                    0.5 * (this->pin[i][j + 1] - this->pin[i][j - 1]);
-                T alpha = xiota.Dot(xiota) + 1.0e-8;
-                T beta = xxi.Dot(xiota);
-                T ganma = xxi.Dot(xxi) + 1.0e-8;
-
-                //.....Update values.....
-                Point<T> tmp = this->pin[i][j];
-                this->pin[i][j] =
-                    (0.5 / (alpha + ganma)) *
-                    (alpha * (this->pin[i + 1][j] + this->pin[i - 1][j]) -
-                     0.5 * beta *
-                         (this->pin[i + 1][j + 1] - this->pin[i - 1][j + 1] -
-                          this->pin[i + 1][j - 1] + this->pin[i - 1][j - 1]) +
-                     ganma * (this->pin[i][j + 1] + this->pin[i][j - 1]));
-
-                //.....Get norm maximam.....
-                T dxtmp = (tmp - this->pin[i][j]).Norm();
-                if (dxmax < dxtmp) {
-                    dxmax = dxtmp;
-                }
-            }
-        }
-        // std::cout << "\t" << dxmax << "\n";
-
-        //.....Check convergence.....
-        if (dxmax < DEPS) {
-            std::cout << "Converged:k = " << k + 1;
-            break;
-        }
-    }
-}
-
-template <class T>
-void EllipticalMesher<T>::ExportToVTK(std::string _fname) {
-    std::ofstream fout(_fname + ".vtk");
-
-    //----------Header�̏o��----------
-    fout << "# vtk DataFile Version 4.1\n";
-    fout << "vtk output\n";
-    fout << "ASCII\n";
-    fout << "DATASET UNSTRUCTURED_GRID\n";
-
-    //----------�_�̒ǉ�----------
-    fout << "\nPOINTS\t" << this->inum * this->jnum << "\tfloat\n";
-    for (auto pinrow : this->pin) {
-        for (auto pinone : pinrow) {
-            fout << pinone[0] << "\t" << pinone[1] << "\t" << 0.0 << "\n";
-        }
-    }
-
-    //----------�v�f�̒ǉ�----------
-    fout << "\nCELLS " << (this->inum - 1) * (this->jnum - 1) << "\t"
-         << (this->inum - 1) * (this->jnum - 1) * 5 << "\n";
-    for (int i = 0; i < this->inum - 1; i++) {
-        for (int j = 0; j < this->jnum - 1; j++) {
-            fout << "4 " << i * (this->jnum) + j << "\t"
-                 << i * (this->jnum) + j + 1 << "\t"
-                 << (i + 1) * (this->jnum) + j + 1 << "\t"
-                 << (i + 1) * (this->jnum) + j << "\n";
-        }
-    }
-
-    //----------�v�f�^�C�v�̐ݒ�----------
-    fout << "\nCELL_TYPES\t" << (this->inum - 1) * (this->jnum - 1) << "\n";
-    for (int i = 0; i < (this->inum - 1) * (this->jnum - 1); i++) {
-        fout << "9\n";
-    }
-
-    fout.close();
-}
-
-template <class T>
-void EllipticalMesher<T>::ExportForPANSFEM(std::string _fnameheader) {
-    //----------Node�̏o��----------
-    std::ofstream fout_node(_fnameheader + "_Node.csv");
-
-    //.....Header�̏o��.....
-    fout_node << "ID,DOX,x0,x1";
-
-    //.....���W���o��.....
-    for (int i = 0; i < this->inum; i++) {
-        for (int j = 0; j < this->jnum; j++) {
-            fout_node << std::endl
-                      << i * (this->jnum) + j << ","
-                      << "2"
-                      << "," << this->pin[i][j].x[0] << ","
-                      << this->pin[i][j].x[1];
-        }
-    }
-
-    fout_node.close();
-
-    //----------Element�̏o��----------
-    std::ofstream fout_element(_fnameheader + "_Element.csv");
-
-    //.....Header�̏o��.....
-    fout_element << "ID,NON,n0,n1,n2,n3";
-
-    //.....�ߓ_�ԍ����o��.....
-    for (int i = 0; i < this->inum - 1; i++) {
-        for (int j = 0; j < this->jnum - 1; j++) {
-            fout_element << std::endl
-                         << i * (this->jnum - 1) + j << ","
-                         << "4"
-                         << "," << i * (this->jnum) + j << ","
-                         << (i + 1) * (this->jnum) + j << ","
-                         << (i + 1) * (this->jnum) + j + 1 << ","
-                         << i * (this->jnum) + j + 1;
-        }
-    }
-
-    fout_element.close();
-}
 }  // namespace EllipticalPAN
--- test/main.cpp
+++ test/main.cpp
@@ -15,69 +15,68 @@
 
 using namespace EllipticalPAN;
 int main() {
-    const double height = 2.0, width = 10.0, lead_offset = 0.5;
+    const double height = 2.0, width = 10.0, offset = 0.5;
     const int trail_num = 70, thick_num = 30, lead_num = 77;
 
     std::ifstream ifs("sample/NACA4412.dat");
     if (!ifs) {
         std::exit(1);
     }
-    std::vector<Point<double>> FoilPoints(0);
+    std::vector<Point<double>> PF(0);
     while (!ifs.eof()) {
         double x, y;
         ifs >> x >> y;
-        FoilPoints.push_back(Point<double>(x, y));
+        PF.push_back(Point<double>(x, y));
     }
 
-    int foil_num = FoilPoints.size(),
-        side_num = trail_num + (foil_num - lead_num) / 2;
+    int foil_num = PF.size(), side_num = trail_num + (foil_num - lead_num) / 2;
 
-    EllipticalMesher<double> Mesh(foil_num + trail_num * 2, thick_num, 1.0e-8);
+    Mesher<double> Mesh(foil_num + trail_num * 2, thick_num);
 
     Point<double> P0(width, 0.0);
     Point<double> P1(width, height);
-    Point<double> P2(lead_offset, height);
-    Point<double> P3(lead_offset, -height);
+    Point<double> P2(offset, height);
+    Point<double> P3(offset, -height);
     Point<double> P4(width, -height);
 
     int ipb = 0;
     for (int i = 0; i < trail_num; i++, ipb++) {
-        Mesh.SetBoundaryPoint(ipb, P0 + (FoilPoints[foil_num - 1] - P0) *
-                                            (double)(i / (double)trail_num));
+        Mesh.SetPoint(ipb, P0 + (PF[foil_num - 1] - P0) *
+                                    (double)(i / (double)trail_num));
     }
     for (int i = foil_num - 1; i >= 0; i--, ipb++) {
-        Mesh.SetBoundaryPoint(ipb, FoilPoints[i]);
+        Mesh.SetPoint(ipb, PF[i]);
     }
     for (int i = 0; i < trail_num - 1; i++, ipb++) {
-        Mesh.SetBoundaryPoint(
-            ipb, FoilPoints[0] + (P0 - FoilPoints[0]) *
-                                     (double)((i + 1) / (double)trail_num));
+        Mesh.SetPoint(
+            ipb, PF[0] + (P0 - PF[0]) * (double)((i + 1) / (double)trail_num));
     }
+
     for (int i = 0; i < thick_num - 1; i++, ipb++) {
-        Mesh.SetBoundaryPoint(
-            ipb, P0 + (P1 - P0) * (double)(i / (double)(thick_num - 1)));
+        Mesh.SetPoint(ipb,
+                      P0 + (P1 - P0) * (double)(i / (double)(thick_num - 1)));
     }
+
     for (int i = 0; i < side_num; i++, ipb++) {
-        Mesh.SetBoundaryPoint(ipb,
-                              P1 + (P2 - P1) * (double)(i / (double)side_num));
+        Mesh.SetPoint(ipb, P1 + (P2 - P1) * (double)(i / (double)side_num));
     }
     for (int i = 0; i < lead_num; i++, ipb++) {
-        Mesh.SetBoundaryPoint(
-            ipb,
-            Point<double>(
-                lead_offset -
-                    height * sin(M_PI * (double)(i / (double)(lead_num - 1))),
-                height * cos(M_PI * (double)(i / (double)(lead_num - 1)))));
+        Mesh.SetPoint(
+            ipb, Point<double>(
+                     offset - height * sin(M_PI * i / (double)(lead_num - 1)),
+                     height * cos(M_PI * i / (double)lead_num - 1)));
     }
     for (int i = 0; i < side_num - 1; i++, ipb++) {
-        Mesh.SetBoundaryPoint(
-            ipb, P3 + (P4 - P3) * (double)((i + 1) / (double)side_num));
+        Mesh.SetPoint(ipb,
+                      P3 + (P4 - P3) * (double)((i + 1) / (double)side_num));
     }
+
     for (int i = 0; i < thick_num - 1; i++, ipb++) {
-        Mesh.SetBoundaryPoint(
-            ipb, P4 + (P0 - P4) * (double)(i / (double)(thick_num - 1)));
+        Mesh.SetPoint(ipb,
+                      P4 + (P0 - P4) * (double)(i / (double)(thick_num - 1)));
     }
-    Mesh.MakeMesh();
+
+    Mesh.Generate();
     Mesh.ExportToVTK("sample/mesh");
     return 0;
 }
